d403e6db1ad971c76cbfdb2b4794dfbc6d11d329
YH <me@yijun.hu>
2024-09-29 20:02:07 +0100

    Add clickable button and hover colour

--- main.py
+++ main.py
@@ -12,17 +12,31 @@ HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Calculator")
 
+button_box = pygame.Rect(40, 80, 80, 40)
+button_colour = (33, 196, 50)  # a green
+button_hover_colour = (85, 217, 98)
+
+
 done = False 
 while not done:
+    # update
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True 
             
-    # update
+        if event.type == pygame.MOUSEBUTTONUP:
+            if button_box.collidepoint(pygame.mouse.get_pos()):
+                print("Clicked!")
+            
     
     
     # draw
     screen.fill(WHITE)
+    
+    if button_box.collidepoint(pygame.mouse.get_pos()):
+        pygame.draw.rect(screen, button_hover_colour, button_box)
+    else:
+        pygame.draw.rect(screen, button_colour, button_box)
     
     pygame.display.flip()
     clock.tick(FPS)
