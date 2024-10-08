6d2b468516cb28b1325f8f5db45af24a0b116e0f
YH <me@yijun.hu>
2024-10-08 20:02:00 +0100

    Make new button and functions to clean up code

--- main.py
+++ main.py
@@ -15,7 +15,30 @@ pygame.display.set_caption("Calculator")
 button_box = pygame.Rect(40, 80, 80, 40)
 button_colour = (33, 196, 50)  # a green
 button_hover_colour = (85, 217, 98)
+button_pressed_colour = (25, 128, 36)
+button_pressed = False
 
+button_box2 = pygame.Rect(140, 80, 80, 40)
+button_colour2 = (186, 39, 39)
+button_hover2_colour = (217, 56, 56)
+button_pressed2_colour = (158, 38, 46)
+button_pressed2 = False 
+
+def mouse_collides_with_box(box:pygame.Rect):
+    return box.collidepoint(pygame.mouse.get_pos())
+
+def draw_button(is_button_pressed, 
+                button_box, 
+                button_colour, 
+                button_hover_colour, 
+                button_pressed_colour,
+                screen):
+    if is_button_pressed:
+        pygame.draw.rect(screen, button_pressed_colour, button_box)
+    elif mouse_collides_with_box(button_box):
+        pygame.draw.rect(screen, button_hover_colour, button_box)
+    else:
+        pygame.draw.rect(screen, button_colour, button_box)
 
 done = False 
 while not done:
@@ -24,19 +47,27 @@ while not done:
         if event.type == pygame.QUIT:
             done = True 
             
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if mouse_collides_with_box(button_box):
+                button_pressed = True
+            
+            if mouse_collides_with_box(button_box2):
+                button_pressed2 = True
+            
         if event.type == pygame.MOUSEBUTTONUP:
-            if button_box.collidepoint(pygame.mouse.get_pos()):
+            button_pressed = False 
+            if mouse_collides_with_box(button_box):
                 print("Clicked!")
             
-    
+            button_pressed2 = False
+            if mouse_collides_with_box(button_box2):
+                print("Clicked 2!")
     
     # draw
     screen.fill(WHITE)
     
-    if button_box.collidepoint(pygame.mouse.get_pos()):
-        pygame.draw.rect(screen, button_hover_colour, button_box)
-    else:
-        pygame.draw.rect(screen, button_colour, button_box)
+    draw_button(button_pressed, button_box, button_colour, button_hover_colour, button_pressed_colour, screen)
+    draw_button(button_pressed2, button_box2, button_colour2, button_hover2_colour, button_pressed2_colour, screen)
     
     pygame.display.flip()
     clock.tick(FPS)
