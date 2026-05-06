612e98cd6132667075218ac6e2e24117bbca5514
yijun <me@yijun.hu>
2026-05-06 21:03:02 +0100

    WIP: make pipes move and geenrate pipes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 from bird import Bird
-from constants import FRAMERATE, HEIGHT, WIDTH
+from constants import FRAMERATE, HEIGHT, PIPE_WIDTH, WIDTH
 from pipe import PipePair
 
 pygame.init()
@@ -12,9 +12,44 @@ clock = pygame.time.Clock()
 
 BIRD_STARTING_X = 480
 BIRD_STARTING_Y = HEIGHT // 2
+GAP_BETWEEN_PIPES = 300
 
+pipe_speed = 3
 bird = Bird(BIRD_STARTING_X, BIRD_STARTING_Y)
 pipes = [PipePair(700, 300, 200)]
+
+# =====
+
+def manage_pipes():
+    """Generation, moving, and deleting of pipes"""
+    # 1. move the pipes
+    for pipe in pipes:
+        pipe.move(pipe_speed)
+
+    # 2. generate the pipes
+    #       - if there are no pipes, we generate a pipe
+    #       - if the pipe gap is big enough we generate a pipe
+    #    IF we generate the pipe 100 px to the right of the screen
+    #    THEN the gap is big enough when the last pipe in sequence is 200 + PIPE_WIDTH past
+
+    # we need to generate pipes
+    # / which means: we need to know when to generate pipes
+    # / which means: either: (a) there's no pipes, or (b) the LAST pipe in the list is 
+    #   GAP_BETWEEN_PIPES + PIPE_WIDTH position away from WIDTH
+
+    can_generate_new_pipe = len(pipes) == 0 or WIDTH - pipes[-1].x_pos >= GAP_BETWEEN_PIPES + PIPE_WIDTH
+    if can_generate_new_pipe:
+        ...
+
+    # we know we CAN generate a pipe now. We need to add a pipe to the END of the pipes list
+    # which means: we need to know how to create a pipe to add
+    # which means: we need to generate the values to create a pipe x_pos:float, gap_y:int, gap_height:int
+
+    # 3. delete unused pipes
+    #       - if a pipe is entirely off the screen
+    #       - if the first pipe in the list os more than 300px off the left side, o r if there's more than 7 pipes
+
+# =======
 
 done = False 
 
@@ -31,6 +66,7 @@ while not done:
     
     # update logic and physics
     bird.update()
+    manage_pipes()
     
     # draw stuff!
     screen.fill("white")  # todo: hex codes
--- pipe.py
+++ pipe.py
@@ -9,7 +9,7 @@ class PipePair:
     gap_y is the top of the gap, gap_height is the size of the gap, the rest 
     of the column are all pipes
     """
-    def __init__(self, x_pos:int, gap_y:int, gap_height:int):
+    def __init__(self, x_pos:float, gap_y:int, gap_height:int):
         self.x_pos = x_pos
         self.gap_y = gap_y
         self.gap_height = gap_height
@@ -19,6 +19,9 @@ class PipePair:
         top, bottom = self.hitbox()
         pygame.draw.rect(screen, "darkgreen", top)
         pygame.draw.rect(screen, "darkgreen", bottom)
+
+    def move(self, speed:float):
+        self.x_pos -= speed
         
 
     def hitbox(self) -> tuple[pygame.Rect, pygame.Rect]:
